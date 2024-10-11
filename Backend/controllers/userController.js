6b969214a1fc301892b4aeaaebf3b46b92d12046
Narendar2024 <narendarkorem471@gmail.com>
2024-10-12 03:19:19 +0530

    forgot password and hashing password have been created successfully

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -4,6 +4,8 @@ const multer = require("multer");
 const upload = multer();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Token = require("../models/TokenModel");
+const crypto = require("crypto");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
@@ -134,7 +136,122 @@ const logout = asyncHandler(async (req, res) => {
 
 // Get User Data
 const getUser = asyncHandler(async (req, res) => {
-  res.send("Get User Data");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
 });
 
-module.exports = { registerUser, loginUser, logout, getUser };
+// Get Login Status
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json(false);
+  }
+
+  // Verify Token
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+  if (verified) {
+    return res.json(true);
+  }
+  return res.json(false);
+});
+
+// Update User
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    const { name, email, photo, phone, bio } = user;
+    user.email = email;
+    user.name = req.body.name || name;
+    user.phone = req.body.phone || phone;
+    user.bio = req.body.bio || bio;
+    user.photo = req.body.photo || photo;
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      photo: updatedUser.photo,
+      phone: updatedUser.phone,
+      bio: updatedUser.bio,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// Change Password
+const changePassword = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  const { oldPassword, password } = req.body;
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, Please singup");
+  }
+  // Validate
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error("Please add old and nre password");
+  }
+
+  // Check if old password matches password in DB
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  // Save new Password
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+    res.status(200).send("password changed successfull");
+  } else {
+    res.status(400);
+    throw new Error("Old Password is Incorrect");
+  }
+});
+
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User does not exist");
+  }
+
+  // Create Reset Token
+  let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+
+  // Hash Token before saving to DB
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  console.log(hashedToken);
+  res.send("Forgot Password");
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logout,
+  getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,11 +5,20 @@ const {
   loginUser,
   logout,
   getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
 } = require("../controllers/userController");
+const protect = require("../middlewares/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
-router.get("/getUser", getUser);
+router.get("/getUser", protect, getUser);
+router.get("/loggedin", loginStatus);
+router.patch("/updateuser", protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
+router.post("/forgotpassword", forgotPassword);
 
 module.exports = router;
